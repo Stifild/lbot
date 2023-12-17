d293c958f932a122e1c685dc9bd834faabfd8df3
Stifild <fedorovkirillwww@yandex.ru>
2023-12-17 18:57:54 +0300

    35

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ token = getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(token)
 
+description_txt = f'{inf.o['name']}\n\n{inf.o["description"]['age']}\n\n{inf.o["description"]['profession']}\n\n{inf.o["description"]['characteristic']}\n\n{inf.o["description"]['hobby']['literature']}\n{inf.o["description"]['hobby']['music']}\n{inf.o['description']['hobby']['sport']}\n{inf.o["description"]['hobby']['social']}\n\n{inf.o["description"]['fashion']}'
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -27,7 +29,8 @@ def send_help(message):
 
 @bot.message_handler(commands=['all_info'])
 def all_info(message):
-    bot.send_photo(chat_id=message.chat.id, photo=inf.o['img'], caption=f'{inf.o['name']}\n{inf.o["description"]}')
+    global description_txt
+    bot.send_photo(chat_id=message.chat.id, photo=inf.o['img'], caption=description_txt)
 
 
 @bot.message_handler(commands=['photo'])
@@ -37,7 +40,8 @@ def photo(message):
 
 @bot.message_handler(commands=['description'])
 def description(message):
-    bot.send_message(chat_id=message.chat.id, text=inf.o['description'])
+    global description_txt
+    bot.send_message(chat_id=message.chat.id, text=description_txt)
 
 
 bot.polling()
